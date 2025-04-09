72fca458f5640583c64cbebeb1df705e582a1727
HSK-DunDunRST <gon5073@naver.com>
2025-04-09 19:16:47 +0900

    Create Project(Default)

---
+++ src/main/java/com/hsk/restapi/HSKSite/HskSiteApplication.java
@@ -0,0 +1,13 @@
+package com.hsk.restapi.HSKSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HskSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HskSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hsk/restapi/HSKSite/HskSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsk.restapi.HSKSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HskSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
